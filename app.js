ba36d7b97cb94a112fc25bf0e37f6a1876196d4c
jekabshincenbergs <jekabs.hincenbergs@nordigen.com>
2020-04-14 20:02:32 +0300

    spotify auth working, simple socket example, tested audio preview urls

--- app.js
+++ app.js
@@ -51,7 +51,7 @@ app.get('/login', function(req, res) {
   res.cookie(stateKey, state);
 
   // your application requests authorization
-  var scope = 'user-read-private user-read-email';
+  var scope = 'user-read-private user-top-read user-read-email streaming';
   res.redirect('https://accounts.spotify.com/authorize?' +
     querystring.stringify({
       response_type: 'code',
